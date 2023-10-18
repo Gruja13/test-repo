7c40936e68f9caf30ba14c05bcdf30ba213adcfa
Djordje Grujovic <djgrujovic13@gmail.com>
2023-10-18 20:34:38 +0200

    Save data in localStorage :)

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,16 +7,31 @@ import { User } from '../models/user';
   providedIn: 'root'
 })
 export class UserService {
-
   private usersSubject: BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
   users$: Observable<User[]> = this.usersSubject.asObservable();
   
   constructor() {
-    this.usersSubject.next([
-      { id: 1, name: 'User 1', active: true },
-      { id: 2, name: 'User 2', active: false },
-      { id: 3, name: 'User 3', active: false },
-    ]);
+    this.loadUsersFromLocalStorage(); // Load users from local storage on initialization
+  }
+
+  // Helper function to load users from local storage
+  private loadUsersFromLocalStorage() {
+    const storedUsers = localStorage.getItem('users');
+    if (storedUsers) {
+      this.usersSubject.next(JSON.parse(storedUsers));
+    } else {
+      // Initialize with some default data if local storage is empty
+      this.usersSubject.next([
+        { id: 1, name: 'User 1', active: true },
+        { id: 2, name: 'User 2', active: false },
+        { id: 3, name: 'User 3', active: false },
+      ]);
+    }
+  }
+
+  // Helper function to save users to local storage
+  private saveUsersToLocalStorage(users: User[]) {
+    localStorage.setItem('users', JSON.stringify(users));
   }
 
   getUsers(): Observable<User[]> {
@@ -30,7 +45,7 @@ export class UserService {
         const currentUsers = this.usersSubject.value;
         const updatedUsers = [...currentUsers, user];
         this.usersSubject.next(updatedUsers);
-
+        this.saveUsersToLocalStorage(updatedUsers); // Save to local storage
         return user;
       })
     );
@@ -42,6 +57,7 @@ export class UserService {
       user.id === updatedUser.id ? updatedUser : user
     );
     this.usersSubject.next(updatedUsers);
+    this.saveUsersToLocalStorage(updatedUsers); // Save to local storage
   }
 
   checkUsernameUniqueness(username: string): Observable<boolean> {
